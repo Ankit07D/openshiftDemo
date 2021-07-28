1da3da2a3e6c32258969f60f09bfb364813661b6
Ankit Dubey <ankit06dubey@gmail.com>
2021-07-28 17:18:20 +0530

    sample spring boot application

---
+++ EAPSample/src/main/java/com/eap/demo/EAPSample/EapSampleApplication.java
@@ -0,0 +1,13 @@
+package com.eap.demo.EAPSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapSampleApplication.class, args);
+	}
+
+}
